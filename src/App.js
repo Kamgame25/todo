b1985ed169b1abf6d3865ae15d381a4777a06bbf
Kamgame25 <kamgame652@gmail.com>
2024-10-04 01:05:43 +0200

    Visual ready, not working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './component/header/header.jsx'
+import Main from './component/main/main.jsx'
 
 function App() {
   return (
-    <>
-    <Header/>
-    </>
+    <body className="bg-white text-gray-900">
+  <div className="flex flex-col min-h-screen">
+        <Header/>
+        <Main/>
+        </div>
+        </body>
   );
 }
 
--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,9 +1,10 @@
 import '../../App.css'
 function Header(){
     return (
-        <header class="flex items-center justify-between p-6 bg-white shadow-md">
+        <header class="flex items-center justify-center gap-24 p-6 bg-white shadow-md">
             <h1 class="text-xl font-semibold">To-Do List</h1>
-            <button class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors">Add Task</button>
+            <input class="  bg-slate-400 placeholder:text-black font-sans font-normal   text-sm px-3 py-2.5 rounded-[7px] focus:bg-blue-300 " placeholder="Write your task"></input>
+            <button class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">Add Task</button>
         </header>
     )
 }
